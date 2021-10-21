bd809d55ffd98da4b0dc23df9f601bdca5bb4ae8
Patrick Jung <patrick.jung@externals.adidas.com>
2021-10-21 13:38:44 +0200

    Add validation logic for mouse actions; Added exception which is thrown in case of errors

--- src/macro-validator.ts
+++ src/macro-validator.ts
@@ -1,4 +1,17 @@
-import { Action, KeyboardAction, Macro, MouseAction } from "./models";
+import { Action, KeyboardAction, Macro, MouseAction, MouseButton, TargetLocation } from "./models";
+
+class MacroValidationError extends Error {
+  private error: String
+
+  constructor(message: String) {
+    super()
+    this.error = message
+  }
+
+  public getError(): String {
+    return this.error
+  }
+}
 
 class MacroValidator {
 
@@ -16,7 +29,42 @@ class MacroValidator {
   }
 
   private validateMouseAction(mouseAction: MouseAction) {
-    // TODO
+    if (mouseAction.action !== 'click' && mouseAction.action !== 'move') {
+      throw new MacroValidationError('Invalid Mouse action type provided!')
+    }
+    if (mouseAction.action == 'click') {
+      this.validateMouseClickAction(mouseAction)
+    } else {
+      this.validateMouseMoveAction(mouseAction)
+    }
+  }
+
+  private validateMouseMoveAction(mouseAction: MouseAction) {
+    if (!mouseAction.location) {
+      throw new MacroValidationError('Invalid mouse move action. Missing required property: location')
+    } else {
+      this.validateTargetLocation(mouseAction.location)
+    }
+  }
+
+  private validateTargetLocation(location: TargetLocation) {
+    if (location.x < 0 || location.y < 0) {
+      throw new MacroValidationError('Location coordinates cannot be negative')
+    }
+  }
+
+  private validateMouseClickAction(mouseAction: MouseAction) {
+    if (!mouseAction.button) {
+      throw new MacroValidationError('Invalid mouse click action. Missing required property: button')
+    } else {
+      this.validateMouseButton(mouseAction.button)
+    }
+  }
+
+  private validateMouseButton(mouseButton: MouseButton) {
+    if (mouseButton !== 'left' && mouseButton !== 'right' && mouseButton !== 'middle') {
+      throw new MacroValidationError('Invalid mouse button provided')
+    }
   }
 
   private validateKeyboardAction(keyboardAction: KeyboardAction) {
