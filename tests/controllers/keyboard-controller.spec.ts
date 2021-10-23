a6119feabd43267b9c949555af7113f1c67b7cec
Patrick Jung <patrick.pddstudio@googlemail.com>
2021-10-23 03:02:52 +0200

    Added test cases for mouse and keyboard controller

--- tests/controllers/keyboard-controller.spec.ts
+++ tests/controllers/keyboard-controller.spec.ts
@@ -1,10 +1,32 @@
 import { KeyboardController } from "../../src"
 
+const mockKey = 'f1'
+const mockText = 'This is some text'
+
 describe('Keyboard Controller Tests', () => {
 
+  var keyboardController: KeyboardController
+
+  beforeEach(() => {
+    keyboardController = new KeyboardController()
+    keyboardController.type = jest.fn()
+    keyboardController.pressKey = jest.fn()
+  })
+
   test('New instance should exist', () => {
-    const keyboardController = new KeyboardController()
     expect(keyboardController).toBeDefined()
+  })
+
+  test('Key press function should be executed', () => {
+    keyboardController.pressKey(mockKey)
+
+    expect(keyboardController.pressKey).toHaveBeenCalledWith(mockKey)
+  })
+
+  test('Type text function should be called', () => {
+    keyboardController.type(mockText)
+
+    expect(keyboardController.type).toHaveBeenCalledWith(mockText)
   })
 
 })
--- tests/controllers/mouse-controller.spec.ts
+++ tests/controllers/mouse-controller.spec.ts
@@ -1,10 +1,34 @@
 import { MouseController } from "../../src"
 
+const mockLocation = { x: 100, y: 150 }
+const mockButton = 'left'
+const mockType = 'single'
+const mockBool = true
+
 describe('Mouse Controller Tests', () => {
 
+  var mouseController: MouseController
+
+  beforeEach(() => {
+    mouseController = new MouseController()
+    mouseController.click = jest.fn()
+    mouseController.move = jest.fn()
+  })
+
   test('New instance should exist', () => {
-    const mouseController = new MouseController()
     expect(mouseController).toBeDefined()
+  })
+
+  test('Mouse move function should be exectued', () => {
+    mouseController.move(mockLocation)
+
+    expect(mouseController.move).toHaveBeenCalledWith(mockLocation)
+  })
+
+  test('Mouse click function should be executed', () => {
+    mouseController.click(mockLocation, mockButton, mockType, mockBool)
+
+    expect(mouseController.click).toHaveBeenCalledWith(mockLocation, mockButton, mockType, mockBool)
   })
 
 })
